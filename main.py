2c2525ec4355661bff707135fb2b31661125a127
PavelAltynnikov <robenzon11@gmail.com>
2023-04-27 21:04:09 +0400

    add Game class

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import random
 from typing import Protocol, Callable
+
 from sveta_hero import SvetaHero
 from hero_class import Hero
 
@@ -15,28 +17,33 @@ class HeroProtocol(Protocol):
     def defense(self):
         ...
 
+    def stop_defense(self):
+        ...
+
     def rest(self):
         ...
 
 
 class Action:
-    def __init__(self, title: str, priority: int, method: Callable[..., None]):
+    def __init__(self, title: str, priority: int, method: Callable[..., None], playerTurn):
         self.title = title
         self.priority = priority
         self.method = method
+        self.player_turn = playerTurn
 
     def __call__(self):
         return self.method()
 
 
 class PlayerTurn:
-    def __init__(self, character: HeroProtocol, enemy: HeroProtocol):
+    def __init__(self, player_name: str, character: HeroProtocol, enemy: HeroProtocol):
+        self.player_name = player_name
         self._character = character
         self._enemy = enemy
         self._actions = {
-            '1': Action('Атаковать', 1, self._attack_action),
-            '2': Action('Защищаться', 2, self._defense_action),
-            '3': Action('Отдыхать', 0, self._rest_action),
+            '1': Action('Атаковать', 1, self._attack_action, self),
+            '2': Action('Защищаться', 2, self._defense_action, self),
+            '3': Action('Отдыхать', 0, self._rest_action, self),
         }
 
     def ask_action(self) -> Action:
@@ -55,39 +62,92 @@ class PlayerTurn:
             print('Не корректный ввод данных. Нужно выбрать один из вариантов')
 
     def _attack_action(self):
-        damage = self._character.deal_damage()
-        self._enemy.take_damage(damage)
+        self._character.stop_defense()
+        self._enemy.take_damage(self._character.deal_damage())
 
     def _defense_action(self):
         self._character.defense()
 
     def _rest_action(self):
+        self._character.stop_defense()
         self._character.rest()
 
 
-halfling = SvetaHero('Серафима Подсветкина', 'Вор', 'Халфлинг')
-warrior = Hero('Волик', 'Воин', 'Человек', 100, 100, 12, 10)
+class Game:
+    def __init__(self, hero_1: SvetaHero, hero_2: Hero):
+        self.hero_1 = hero_1
+        self.hero_2 = hero_2
+        self.turn_hero_1 = PlayerTurn('Света', hero_1, hero_2)  # type: ignore
+        self.turn_hero_2 = PlayerTurn('Иван', hero_2, hero_1)  # type: ignore
+        self.winner = None
+        self.is_game_over = False
 
-sveta_turn = PlayerTurn(halfling, warrior)  # type: ignore
-ivan_turn = PlayerTurn(warrior, halfling)  # type: ignore
+    def show_intro(self):
+        print('Добро пожаловать на поединок боевых героев созданных Иваном и Светой!!!')
 
-game_over = False
+    def start_game_loop(self):
+        while not self.is_game_over:
+            self._show_characters_characteristics()
 
-print('Добро пожаловать на поединок боевых героев созданных Иваном и Светой!!!')
+            first_action, second_action = self._assign_actions_order(
+                self.turn_hero_1.ask_action(),
+                self.turn_hero_2.ask_action()
+            )
 
-while not game_over:
-    print(f'\nХарактеристики персонажей\n\n{halfling}\n\n{warrior}')
+            self._make_a_move(first_action, 'первым')
+            self._assign_winner()
 
-    sveta_action = sveta_turn.ask_action()
-    ivan_action = ivan_turn.ask_action()
+            self._make_a_move(second_action, 'вторым')
+            self._assign_winner()
 
-    if sveta_action.priority > ivan_action.priority:
-        print(f'\n{halfling.name} будет {sveta_action.title} первым')
-        sveta_action()
-        print(f'{warrior.name} будет {ivan_action.title} вторым')
-        ivan_action()
-    else:
-        print(f'\n{warrior.name} будет {ivan_action.title} первым')
-        ivan_action()
-        print(f'{halfling.name} будет {sveta_action.title} вторым')
-        sveta_action()
+        self._announce_winner()
+        self._show_characters_characteristics()
+
+    def _assign_actions_order(self, action_1, action_2):
+        if action_1.priority == action_2.priority:
+            return random.sample((action_1, action_2), 2)
+
+        if action_1.priority > action_2.priority:
+            return action_1, action_2
+
+        return action_2, action_1
+
+    def _make_a_move(self, action: Action, order: str):
+        self._announce_action(action, order)
+        action()
+
+    def _announce_action(self, action: Action, order: str):
+        print(f'\n{action.player_turn.player_name} будет {action.title} {order}')
+
+    def _assign_winner(self):
+        if self.hero_1.health != 0 and self.hero_2.hp != 0:
+            return
+
+        if self.hero_1.health == 0:
+            self.winner = self.hero_2
+        else:
+            self.winner = self.hero_1
+
+        self.is_game_over = True
+
+    def _announce_winner(self):
+        if self.winner:
+            print(f'\nВыиграл {self.winner.name}')
+        else:
+            print('Ничья')
+
+    def _show_characters_characteristics(self):
+        print(f'\nХарактеристики персонажей\n\n{self.hero_1}\n\n{self.hero_2}')
+
+
+def main():
+    halfling = SvetaHero('Серафима Подсветкина', 'Вор', 'Халфлинг')
+    warrior = Hero('Волик', 'Воин', 'Человек', 100, 100, 12, 10)
+
+    game = Game(halfling, warrior)
+    game.show_intro()
+    game.start_game_loop()
+
+
+if __name__ == '__main__':
+    main()
